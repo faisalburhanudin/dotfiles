d6c10df51758324d76fd94a83a089fbafc520483
faisalburhanudin <faisalburhanudin@gmail.com>
2024-02-17 20:13:40 +0700

    remove dap

--- nvim/init.lua
+++ nvim/init.lua
@@ -45,54 +45,6 @@ require("lazy").setup({
   },
 	},
 
-	-- DAP
-	{
-		"mfussenegger/nvim-dap",
-	},
-	{
-		"pocco81/dap-buddy.nvim",
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-	},
-	{
-		"theHamsta/nvim-dap-virtual-text",
-	},
-	{
-		"nvim-neotest/neotest-go",
-	},
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		requires = {
-			"nvim-neotest/neotest-go",
-			-- Your other test adapters here
-		},
-		config = function()
-			-- get neotest namespace (api call creates or returns namespace)
-			local neotest_ns = vim.api.nvim_create_namespace("neotest")
-			vim.diagnostic.config({
-				virtual_text = {
-					format = function(diagnostic)
-						local message =
-							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-						return message
-					end,
-				},
-			}, neotest_ns)
-			require("neotest").setup({
-				-- your neotest config here
-				adapters = {
-					require("neotest-go"),
-				},
-			})
-		end,
-	},
-
 	-- Automatically close tags
 	{
 		"windwp/nvim-autopairs",
