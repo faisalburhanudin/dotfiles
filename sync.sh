8d9aaa5c9e1c1b45a42fced74e750c4fc01ad2e0
faisalburhanudin <faisalburhanudin@gmail.com>
2024-02-17 20:04:42 +0700

    restructure

--- sync.sh
+++ sync.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # skhdrc
-ln -sf "$HOME/workspace/src/github.com/faisalburhanudin/dotfiles/dot-skhdrc" "$HOME/.skhdrc"
+ln -sf "$HOME/workspace/src/github.com/faisalburhanudin/dotfiles/skhdrc" "$HOME/.skhdrc"
+
+# alacritty
+ln -sf "$HOME/workspace/src/github.com/faisalburhanudin/dotfiles/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
