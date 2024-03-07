126b4e87ed5f7bd2fda07aaf4321cb48f3497546
faisalburhanudin <faisalburhanudin@gmail.com>
2024-03-07 16:37:23 +0700

    x

--- nvim/init.lua
+++ nvim/init.lua
@@ -700,6 +700,37 @@ vim.api.nvim_set_keymap("n", "<leader>-", ":split<CR>", { noremap = true, silent
 -- disable pair
 vim.keymap.set("n", "<leader>dp", require("nvim-autopairs").disable, { desc = "Disable [P]air" })
 
+function ExecuteCurrentFile()
+	--  get the current filetype
+	local filetype = vim.bo.filetype
+
+	-- get current file path
+	local file = vim.fn.expand("%:p")
+
+	--  if the filetype is python
+	if filetype == "python" then
+		local venv = os.getenv("VIRTUAL_ENV")
+		if venv then
+			file = venv .. "/bin/python " .. file
+		else
+			file = "python3 " .. file
+		end
+
+		--  execute the current file
+		vim.cmd("w")
+
+		-- execute in term
+		vim.cmd("terminal " .. file)
+	end
+
+	if filetype == "sh" then
+		vim.cmd("w")
+		vim.cmd("terminal bash " .. file)
+	end
+end
+
+vim.keymap.set("n", "<leader>xf", ExecuteCurrentFile, { desc = "E[x]ecute [F]ile" })
+
 local util = require("lspconfig.util")
 
 lspconfig.dartls.setup({
