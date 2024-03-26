82faab3fc900c866f6954189cbb369162f201512
faisalburhanudin <faisalburhanudin@gmail.com>
2024-03-26 16:21:45 +0700

    x

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
 	"stevearc/conform.nvim",
 	"folke/neodev.nvim",
 	"folke/zen-mode.nvim",
+	"leoluz/nvim-dap-go",
 
 	-- Git related plugins
 	"tpope/vim-fugitive",
@@ -703,6 +704,9 @@ vim.keymap.set("n", "<leader>dp", require("nvim-autopairs").disable, { desc = "D
 -- find and Replace
 vim.api.nvim_set_keymap("n", "<leader>fr", ":%s//g<Left><Left>", { noremap = true, silent = true })
 
+-- save
+vim.api.nvim_set_keymap("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
+
 function ExecuteCurrentFile()
 	--  get the current filetype
 	local filetype = vim.bo.filetype
@@ -755,25 +759,45 @@ lspconfig.dartls.setup({
 	},
 })
 
-local dap = require("dap")
+-- local dap = require("dap")
 
-dap.adapters.delve = {
-	type = "server",
-	port = "${port}",
-	executable = {
-		command = "dlv",
-		args = { "dap", "-l", "127.0.0.1:${port}" },
+require("dap-go").setup({
+	-- Additional dap configurations can be added.
+	-- dap_configurations accepts a list of tables where each entry
+	-- represents a dap configuration. For more details do:
+	-- :help dap-configuration
+	dap_configurations = {
+		{
+			-- Must be "go" or it will be ignored by the plugin
+			type = "go",
+			name = "Attach remote",
+			mode = "remote",
+			request = "attach",
+		},
 	},
-}
-
-dap.configurations.go = {
-	{
-		type = "delve",
-		name = "Debug",
-		request = "launch",
-		program = "${file}",
+	-- delve configurations
+	delve = {
+		-- the path to the executable dlv which will be used for debugging.
+		-- by default, this is the "dlv" executable on your PATH.
+		path = "dlv",
+		-- time to wait for delve to initialize the debug session.
+		-- default to 20 seconds
+		initialize_timeout_sec = 20,
+		-- a string that defines the port to start delve debugger.
+		-- default to string "${port}" which instructs nvim-dap
+		-- to start the process in a random available port
+		port = "2345",
+		-- additional args to pass to dlv
+		args = {},
+		-- the build flags that are passed to delve.
+		-- defaults to empty string, but can be used to provide flags
+		-- such as "-tags=unit" to make sure the test suite is
+		-- compiled during debugging, for example.
+		-- passing build flags using args is ineffective, as those are
+		-- ignored by delve in dap mode.
+		build_flags = "",
 	},
-}
+})
 
 -- require("dapui").setup()
 -- vim.keymap.set("n", "<leader>dd", require("dapui").toggle, { desc = "[D]ebugger [D]isplay" })
