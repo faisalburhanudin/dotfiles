625ae5300cd11901155b53d44690254195352ca2
faisalburhanudin <faisalburhanudin@gmail.com>
2024-11-19 10:11:39 +0700

    refactor: make it more reliable by using single node for everything

--- nvim/lua/custom/snippets/go.lua
+++ nvim/lua/custom/snippets/go.lua
@@ -1,19 +1,42 @@
-local ts_utils = require("nvim-treesitter.ts_utils")
 local ls = require("luasnip")
 local s = ls.s
 local i = ls.insert_node
 local t = ls.text_node
 local f = ls.function_node
 
+-- snippet always used at buffer 0
+local bufnr = 0
+
 local function get_node_text(node)
 	return vim.treesitter.get_node_text(node, 0)
 end
 
-local function get_caller()
-	local node = ts_utils.get_node_at_cursor()
-	if not node then
-		return
-	end
+--- Get TSNode for [err]
+--- example:
+---    err := foo()
+--- it will locate TSNode at [err]
+--- @return TSNode?
+local function get_node_at_err()
+	-- we cannot use ts_utils.get_node_at_cursor because when snippet running, TSNode is not yet created
+	-- this will make block node selected, so we need to locate it with position
+	local cursor_pos = vim.api.nvim_win_get_cursor(bufnr)
+
+	-- TSNode index is start from 0
+	-- Lua index is start from 1
+	-- thats why we need to substract with -2 to get row above
+	local row = cursor_pos[1] - 2
+	local col = cursor_pos[2]
+
+	local parser = vim.treesitter.get_parser(bufnr)
+	local tree = parser:parse()[1]
+	local root = tree:root()
+
+	local node = root:named_descendant_for_range(row, col, row, col)
+	return node
+end
+
+local function get_caller(err_node)
+	local node = err_node
 
 	while node do
 		if node:type() == "function_declaration" then
@@ -27,22 +50,9 @@ local function get_caller()
 	return ""
 end
 
-local function get_node_by_position(bufnr, row, col)
-	local parser = vim.treesitter.get_parser(bufnr)
-	local tree = parser:parse()[1]
-	local root = tree:root()
-
-	local node = root:named_descendant_for_range(row - 1, col, row - 1, col)
-	return node
-end
-
-local function get_function_name()
-	local cursor_pos = vim.api.nvim_win_get_cursor(0)
-	local row = cursor_pos[1]
-	local col = cursor_pos[2]
-
-	local node = get_node_by_position(0, row - 1, col)
-
+--- @param err_node TSNode
+local function get_function_name(err_node)
+	local node = err_node
 	while node do
 		if node:type() == "short_var_declaration" then
 			local right_node = node:field("right")[1]
@@ -68,7 +78,15 @@ local function get_function_name()
 end
 
 local function err_handler(_, _, _)
-	local msg = '"error at ' .. get_caller() .. "() when call " .. get_function_name() .. '() error: %w", err'
+	local err_node = get_node_at_err()
+	if err_node == nil then
+		return
+	end
+
+	local caller = get_caller(err_node)
+	local function_name = get_function_name(err_node)
+
+	local msg = '"error at ' .. caller .. "() when call " .. function_name .. '() error: %w", err'
 	return "log.Fatalf(" .. msg .. ")"
 end
 
