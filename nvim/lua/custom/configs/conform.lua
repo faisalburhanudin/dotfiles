60d51d97896352b31a6eea1f43f7ad5d5dda1bae
faisalburhanudin <faisalburhanudin@gmail.com>
2024-11-19 13:02:11 +0700

    feat: formater json & remove snippet vscode

--- nvim/lua/custom/configs/conform.lua
+++ nvim/lua/custom/configs/conform.lua
@@ -2,8 +2,9 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		python = { "isort", "black" },
-		javascript = { { "prettierd", "prettier" } },
+		javascript = { "prettierd", "prettier" },
 		sh = { "shfmt" },
 		go = { "goimports", "gofmt" },
+		json = { "jq" },
 	},
 })
--- nvim/lua/custom/configs/luasnip.lua
+++ nvim/lua/custom/configs/luasnip.lua
@@ -1,7 +1,5 @@
-local loaders = require("luasnip.loaders.from_vscode")
 local from_lua = require("luasnip.loaders.from_lua")
 
-loaders.lazy_load()
 from_lua.load({
 	paths = { "~/.config/nvim/lua/custom/snippets" },
 })
