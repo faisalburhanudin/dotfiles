86bf50bb398a8082da401379dac937d0a4af6e6a
faisalburhanudin <faisalburhanudin@gmail.com>
2024-11-20 11:42:16 +0700

    plugin: Markdown live preview

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -280,4 +280,18 @@ return {
 		},
 		build = ":TSUpdate",
 	},
+
+	-- Markdown viewer
+	-- Start the preview :MarkdownPreview
+
+	-- Stop the preview  :MarkdownPreviewStop
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 }
