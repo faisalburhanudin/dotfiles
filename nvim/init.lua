bbf4fa611e02a953e05da384c386286204b70827
faisalburhanudin <faisalburhanudin@gmail.com>
2024-11-28 13:27:40 +0700

    x

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,9 +27,6 @@ require("custom.snippets")
 require("custom.keymaps")
 require("custom.commands")
 
--- per-projects config
-require("custom.projects")
-
 -- load all plugins configs
 local function plugin_configs()
 	local path = vim.fn.stdpath("config") .. "/lua/custom/configs"
