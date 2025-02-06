60dc61ddace8c6a9f1edd56381251abfe5402fcf
faisalburhanudin <faisalburhanudin@gmail.com>
2025-02-06 15:17:15 +0700

    fix typos error and yank filepath:line

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -25,19 +25,27 @@ set("n", "<leader>o", oil.open)
 -- Telescope
 local builtin = require("telescope.builtin")
 nmap("<leader><space>", builtin.buffers, "[ ] Find existing buffers")
-nmap("<leader>s/", builtin.current_buffer_fuzzy_find, "[S]earch [/] in Open Files")
-nmap("<leader>ss", builtin.builtin, "[S]earch [S]elect Telescope")
-nmap("<leader>gf", builtin.git_files, "Search [G]it [F]iles")
-nmap("<leader>sh", builtin.help_tags, "[S]earch [H]elp")
-nmap("<leader>sw", builtin.grep_string, "[S]earch current [W]ord")
-nmap("<leader>sg", builtin.live_grep, "[S]earch by [G]rep")
-nmap("<leader>sd", builtin.diagnostics, "[S]earch [D]iagnostics")
-nmap("<leader>sr", builtin.resume, "[S]earch [R]esume")
-nmap("<leader>sf", builtin.find_files, "[S]earch [F]iles")
-nmap("<leader>su", builtin.git_status, "[S]earch [U]nstash")
+nmap("<leader>s/", builtin.current_buffer_fuzzy_find, "Search [/] in Open Files")
+nmap("<leader>ss", builtin.builtin, "Search Select Telescope")
+nmap("<leader>gf", builtin.git_files, "Search Git Files")
+nmap("<leader>sh", builtin.help_tags, "Search Help")
+nmap("<leader>sw", builtin.grep_string, "Search current Word")
+nmap("<leader>sg", builtin.live_grep, "Search by Grep")
+nmap("<leader>sd", builtin.diagnostics, "Search Diagnostics")
+nmap("<leader>sr", builtin.resume, "Search Resume")
+nmap("<leader>sf", builtin.find_files, "Search Files")
+nmap("<leader>su", builtin.git_status, "Search Unstash")
 
 -- yank to system clipboard
 set("v", "<leader>y", '"+y')
+
+-- yank to system clipboard, filepath and linenubmer, example: hello.go:123
+set("n", "<leader>fl", function()
+	local path = vim.fn.expand("%:p")
+	local line = vim.fn.line(".")
+	local text = string.format("%s:%d", path, line)
+	vim.fn.setreg("+", text)
+end, { desc = "Yank filepath:line to clipboard" })
 
 -- Diagnostic keymaps
 nmap("[d", vim.diagnostic.goto_prev, "Go to previous diagnostic message")
@@ -47,17 +55,17 @@ nmap("<leader>q", vim.diagnostic.setloclist, "Open diagnostics list")
 
 -- LSP
 local buf = vim.lsp.buf
-nmap("<leader>rn", buf.rename, "[R]e[n]ame")
-nmap("<leader>ca", buf.code_action, "[C]ode [A]ction")
-nmap("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
-nmap("gr", builtin.lsp_references, "[G]oto [R]eferences")
-nmap("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
-nmap("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
-nmap("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
-nmap("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+nmap("<leader>rn", buf.rename, "Rename")
+nmap("<leader>ca", buf.code_action, "Code Action")
+nmap("gd", builtin.lsp_definitions, "Goto Definition")
+nmap("gr", builtin.lsp_references, "Goto References")
+nmap("gI", builtin.lsp_implementations, "Goto Implementation")
+nmap("<leader>D", builtin.lsp_type_definitions, "Type Definition")
+nmap("<leader>ds", builtin.lsp_document_symbols, "Document Symbols")
+nmap("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "Workspace Symbols")
 nmap("K", buf.hover, "Hover Documentation")
 nmap("<C-k>", buf.signature_help, "Signature Documentation")
-nmap("gD", buf.declaration, "[G]oto [D]eclaration")
+nmap("gD", buf.declaration, "Goto Declaration")
 
 -- NOTE: builtin keymaps I always forget
 -- gf	: Open file Under cursor
