f4256f6dd3d3c5f5008cd80c7d8cae4660eb60c2
faisalburhanudin <faisalburhanudin@gmail.com>
2025-03-11 09:51:14 +0700

    fix: upgrade

--- nvim/lua/custom/configs/lsp.lua
+++ nvim/lua/custom/configs/lsp.lua
@@ -3,6 +3,9 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local lspconfig = require("lspconfig")
 
+-- golang
+lspconfig.gopls.setup({})
+
 lspconfig.golangci_lint_ls.setup({
 	capabilities = capabilities,
 	filetypes = { "go", "gomod" },
@@ -20,6 +23,26 @@ lspconfig.golangci_lint_ls.setup({
 	},
 })
 
+-- python
+lspconfig.pyright.setup({})
+
+-- html
+lspconfig.html.setup({})
+
+-- bash
+lspconfig.bashls.setup({})
+
+-- lua
+lspconfig.lua_ls.setup({})
+
+-- typos
+lspconfig.typos_lsp.setup({
+	init_options = {
+		config = "~/.config/typos/typos.toml",
+	},
+})
+
+-- ruby
 lspconfig.ruby_lsp.setup({
 	capabilities = capabilities,
 	cmd = { "/Users/faisal/.rbenv/shims/ruby-lsp" },
